3cd1c0035fc00752342eed24549558d070e32980
r <rm>
2020-08-20 00:21:24 +0100

    Added blank nunit project

---
+++ Pharmacy2URyanMoir/RestApiTests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using Pharmacy2URyanMoir.Controllers;
+
+namespace RestApiTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
