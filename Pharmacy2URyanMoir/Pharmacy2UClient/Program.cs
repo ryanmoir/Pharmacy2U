91eb0494183a3dd00bce5a3474149358e92bfd79
r <rm>
2020-08-19 20:04:50 +0100

    Added blank API and console projects
    Added blank API and console projects. EntityFramework design sqlserver and tools added to the blank API project.

---
+++ Pharmacy2URyanMoir/Pharmacy2UClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pharmacy2UClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
